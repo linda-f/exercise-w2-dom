0e5818d55c0713ecbe2703ce312053eb3e4caa42
unknown <linda.frischknecht@bluewin.ch>
2024-02-11 17:53:58 +0100

    finished the main goals

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log("Start Here 😉");
+
+const box = document.getElementById ("boxjs")
+const boxdark = document.getElementById ("boxjs")
+const light = document.getElementById ("light")
+const dark = document.getElementById ("dark")
+
+light.onclick = () => {
+    box.classList.add("box")
+    box.classList.remove("boxdark")
+    box.classList.remove("crazy")
+}
+
+dark.onclick = () => {
+    box.classList.add("boxdark")
+    box.classList.remove("box")
+    box.classList.remove("crazy")
+}
+
+crazy.onclick = () => {
+    box.classList.add("crazy")
+    box.classList.remove("boxdark")
+    box.classList.remove("box")
+}
+
+
+
+
+
